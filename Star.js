196d3924e9f7fb18c1b8cf4fd9991c51031bb2ca
AdityaSawant0912 <arsawant09@gmail.com>
2021-12-10 21:23:45 +0530

    updated colors

--- Star.js
+++ Star.js
@@ -21,7 +21,7 @@ function Star() {
     var sx = map(this.x / this.z, 0, 1, 0, width);
     var sy = map(this.y / this.z, 0, 1, 0, height);
 
-    var r = map(this.z, 0, width, 16, 0);
+    var r = map(this.z, 0, width, random(1, 16), 0);
     ellipse(sx, sy, r, r);
 
     var px = map(this.x / this.pz, 0, 1, 0, width);
@@ -29,7 +29,7 @@ function Star() {
 
     this.pz = this.z;
 
-    stroke(255);
+    stroke(0, 0, 205);
     line(px, py, sx, sy);
   };
 }
